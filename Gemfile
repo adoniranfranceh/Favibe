aa3e798e14a09592e3045d8c3e0abc1183d24d4b
adoniranfranceh <adoniranfranca@gmail.com>
2024-04-16 18:38:43 -0300

    Adiciona primeira logo com estilização inicial

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby "3.2.2"
 gem "rails", "~> 7.1.3"
 
 gem "bootstrap"
+gem "importmap-rails"
 gem "sprockets-rails"
 gem "sassc-rails"
 gem 'pg'
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,6 @@
-pin "popper", to: 'popper.js', preload: true
+# Pin npm packages by running ./bin/importmap
 
+pin "application"
+
+pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
