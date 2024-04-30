240742469fb88866084c39d6fd5aa307180bc416
adoniranfranceh <adoniranfranca@gmail.com>
2024-04-30 10:36:16 -0300

    Visitante vê apenas eventos públicos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.published
   end
 end
--- spec/system/home/user_view_initial_page_spec.rb
+++ spec/system/home/user_view_initial_page_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
-describe 'Visitante vê eventos' do
-  it 'com sucesso' do
-    create(:event, name: 'Lual das Estrelas')
-    create(:event, name: 'Conferência IA CHIP')
+describe 'Visitante vê tela inicial' do
+  it 'e vê apenas eventos públicos' do
+    create(:event, name: 'Lual das Estrelas', status: :published)
+    create(:event, name: 'Conferência IA CHIP', status: :published)
+    create(:event, name: 'Evento a ser lançado', status: :hidden)
 
     visit root_path
 
     expect(page).to have_content('Lual das Estrelas')
     expect(page).to have_content('Conferência IA CHIP')
+    expect(page).not_to have_content('Evento a ser lançado')
   end
 
   it 'e não existem eventos' do
